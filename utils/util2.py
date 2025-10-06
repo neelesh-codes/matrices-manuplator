9937b6e8167e734a1507eb3eb9d06bfc766ebb27
Neelesh <giroh381@gmail.com>
2025-10-06 14:09:53 +0530

    done some work on util2.py today

--- utils/util2.py
+++ utils/util2.py
@@ -38,28 +38,39 @@ class NPY_file_changer(util1.Numpy_array_manuplator):
             npy_file_path: The path of the file that to load."""
         print(
             f"We are going to show the basic info of the file:\n{npy_file_path}")
-        npy_file: np.array = np.load(npy_file_path)
+        npy_file: np.array = np.load(npy_file_path, allow_pickle=True)
         print("Loaded file successfully!")
         print("Here is the basic info: ")
         print("+--------------+------------------+")
         print(npy_file)
 
         print("Here is the basics info: ")
-        print(f"Max va;lue{np.max(npy_file)} ")
+        print(f"Max va;lue {np.max(npy_file)} ")
         print(f"Min Value: {np.min(npy_file)} ")
         print(f"Sum of all numbers of npy array {np.sum(npy_file)} ")
         print(f"The Data Type of the array: {npy_file.dtype} ")
         print(f"The shape of the array is: {npy_file.shape}")
         print(f"The size of the array is: {npy_file.size}")
 
+    def move_to_npy(self, data: np.array, file_path: str):
+        """This function converts a array or a goroup of arrays into a .npy file.
+        args:
+            self: default argument for object.
+            data: The data to store in the npy file.
+            file_path: name of the .npy file in which data to be stored."""
+        np.save(file_path, data)
+        print(f"Saved the data to {file_path}.")
+        print("The data is: ")
+        file_data = np.load(file_path)
+        print(file_data)
 
 if __name__ == "__main__":
     try:
         instance = NPY_file_changer()
+        instance.basic_info_of_file("E:\\Agents\\Python\\projects\\matrices manuplator\\x.npy")
     except Exception as e:
         print(f"The error is {instance}")
     else:
         print("Passed successfuly.")
     finally:
         print('Exiting the programm!')
-    
